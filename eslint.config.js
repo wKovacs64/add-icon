d8320a33028763cd51a84e6e7522261dd29942b5
Justin Hall <justin.r.hall@gmail.com>
2025-04-13 12:13:16 -0600

    Lint and package.json tweaks

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,3 @@
-import wkovacs64 from '@wkovacs64/eslint-config';
+import config from '@wkovacs64/eslint-config';
 
-export default [
-  ...wkovacs64,
-  {
-    ignores: ['dist/', 'examples/', 'node_modules/', 'example-*.ts'],
-    files: ['src/**/*.ts'],
-  },
-];
+export default config;
