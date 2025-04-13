bf3a083fe6c51e4ccbf83350b9e2b658f74a20af
Justin Hall <justin.r.hall@gmail.com>
2025-04-13 12:00:39 -0600

    Remove examples directory
    Removed the examples directory and its contents, which were used for testing
    the library functionality during development. These files are not needed for
    the final package as the README already contains comprehensive examples.

--- examples/import-test.js
+++
@@ -1,7 +0,0 @@
-// ESM test for importing from iconify-cli
-import { transforms } from 'iconify-cli';
-import { TransformArgs, VERSION } from 'iconify-cli/types';
-
-console.log('Imported transforms:', Object.keys(transforms));
-console.log('TransformArgs type imported successfully');
-console.log('Version:', VERSION);
